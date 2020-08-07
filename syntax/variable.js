e49c78e3184e669ff55f874006b29b7704f37cc7
sandy-yeseul <yeseulkang0328@gmail.com>
2020-08-07 15:45:42 +0900

    js syntax - using variable

---
+++ syntax/variable.js
@@ -0,0 +1,9 @@
+var a = 1;
+console.log(a);
+a=2;
+console.log(a);
+//1==2; comment
+
+var name='egoing';
+var letter= 'Dear '+name+'Lorem ipsum dolor sit '+name+' amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna egoing aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit '+name+' esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat egoing non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+console.log(letter);
