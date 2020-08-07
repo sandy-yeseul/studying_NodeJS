16eba8d6daf24ab8980d0b1b775e6747f8ff9b7a
sandy-yeseul <yeseulkang0328@gmail.com>
2020-08-07 16:48:06 +0900

    js syntax - template

---
+++ syntax/template.js
@@ -0,0 +1,9 @@
+var name='egoing';
+var letter= 'Dear '+name+'\n\nLorem ipsum dolor sit '+name+' amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna egoing aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit '+name+' esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat egoing non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+console.log(letter);
+
+var name='egoing';
+var letter= `Dear ${name}
+
+Lorem ipsum dolor sit ${name} amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna egoing aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit ${name} esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat egoing non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.${name}`;
+console.log(letter);
